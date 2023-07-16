5e19cb61af558a602d79479b5f1ec9d8911fb96b
Nuni Nuraini <nuninuraini@outlook.co.id>
2023-07-16 11:52:19 +0700

    add authentication features

--- src/server.js
+++ src/server.js
@@ -4,9 +4,13 @@ const hapi = require('@hapi/hapi')
 const notes = require('./api/notes')
 const NotesValidator = require('./validator/notes')
 const { NotesService } = require('./services/postgres/notesServices')
+const users = require('./api/users')
+const UsersService = require('./services/postgres/usersServices')
+const UsersValidator = require('./validator/users')
 
 const init = async () => {
     const notesService = new NotesService()
+    const usersService = new UsersService()
     const server = hapi.server({
         port: process.env.PORT,
         host: process.env.HOST,
@@ -17,13 +21,23 @@ const init = async () => {
         }
     })
 
-    await server.register({
-        plugin: notes,
-        options: {
-            service: notesService,
-            validator: NotesValidator
+    await server.register([
+        {
+            plugin: notes,
+            options: {
+                service: notesService,
+                validator: NotesValidator,
+            },
         },
-    })
+        {
+            plugin: users,
+            options: {
+                service: usersService,
+                validator: UsersValidator,
+            },
+        },
+    ])
+     
      
     await server.start()
     console.log(`Server berjalan pada ${server.info.uri}`)
